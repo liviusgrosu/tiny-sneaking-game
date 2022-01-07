731800f7bbee1a925b4f1fdb7836a261d0dfd92b
liviusgrosu <livius.grosu@ryerson.ca>
2022-01-06 21:49:33 -0500

    Added UDP and see through wall shader

---
+++ Assets/Scripts/CircleSync.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleSync : MonoBehaviour
+{
+    public static int PosID = Shader.PropertyToID("_Position");
+    public static int SizeD = Shader.PropertyToID("_Size");
+
+    public Material WallMaterial;
+    public LayerMask Mask;
+
+    void Update()
+    {
+        Vector3 dir = Camera.main.transform.position - transform.position;
+        Ray ray = new Ray(transform.position, dir.normalized);
+
+        if (Physics.Raycast(ray, 3000, Mask))
+        {
+            WallMaterial.SetFloat(SizeD, 1);
+        }
+        else
+        {
+            WallMaterial.SetFloat(SizeD, 0);
+        }
+    }
+}
