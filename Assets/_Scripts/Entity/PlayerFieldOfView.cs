d4dc31a175f5f1c92459270f1fa2aa3662275878
liviusgrosu <livius.grosu@ryerson.ca>
2022-01-23 18:54:24 -0500

    FOV - Reveal objects to the player when its inside their FOV

--- Assets/_Scripts/Entity/PlayerFieldOfView.cs
+++ Assets/_Scripts/Entity/PlayerFieldOfView.cs
@@ -8,9 +8,12 @@ public class PlayerFieldOfView : MonoBehaviour
     [SerializeField] private LayerMask _targetMask;
     [SerializeField] private LayerMask _obstructionMask;
 
+    private List<GameObject> _visibleObjects;
+
     // Start is called before the first frame update
     void Start()
     {
+        _visibleObjects = new List<GameObject>();
         FOV = GetComponent<FieldOfView>();
         FOV.Initialize();
         StartCoroutine(FOVRoutine());
@@ -31,6 +34,31 @@ public class PlayerFieldOfView : MonoBehaviour
     {
         Collider[] rangeChecks = Physics.OverlapSphere(transform.position, FOV.FarRadius, _targetMask);
         
-        
+        if (rangeChecks.Length != 0)
+        {
+            foreach(Collider col in rangeChecks)
+            {
+                // Get distance to object
+                Transform target = col.transform;
+                Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+                // Check if object is within the viewing angle
+                if (Vector3.Angle(transform.forward, directionToTarget) < FOV.angle / 2)
+                {
+                    float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                    // If not obstructions present then the object is clearly visible to the player
+                    if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, _obstructionMask))
+                    {
+                        // Reveal that object
+                        if (!_visibleObjects.Contains(col.gameObject))
+                        {
+                            _visibleObjects.Add(col.gameObject);
+                            col.gameObject.GetComponent<Hideable>().ToggleState(true);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
