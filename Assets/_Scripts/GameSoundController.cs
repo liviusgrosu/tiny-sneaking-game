286a58b6b0a321c76eba4f93ca8f551b0e7fed45
liviusgrosu <livius.grosu@ryerson.ca>
2022-01-25 21:39:21 -0500

    Audio - Adjusted volume of sound depending on its distance and obstacles to player

--- Assets/_Scripts/GameSoundController.cs
+++ Assets/_Scripts/GameSoundController.cs
@@ -6,18 +6,24 @@ using UnityEngine;
 
 public class GameSoundController : MonoBehaviour
 {
-    [SerializeField] private GameObject SoundInstance;
-    // [SerializeField] private AudioClip WoodStep, CarpetStep, StoneStep;
+    [SerializeField] private GameObject _soundInstance;
+    [SerializeField] private LayerMask _obstacleMask;
+    [SerializeField] private float _maxDistToPlayer;
+    private GameManager _gameManager;    
 
     [Serializable]
     public struct Clips
     {
         public string name;
         public AudioClip audioClip;
-        public int size;
     }
 
     [SerializeField] private Clips[] _clips;
+
+    private void Awake()
+    {
+        _gameManager = GetComponent<GameManager>();
+    }
 
     public void PlaySound(string name, Vector3 position)
     {
@@ -29,8 +35,22 @@ public class GameSoundController : MonoBehaviour
                 // Get the clip to use
                 clipToUse = currentClip.audioClip;
                 // Create the sound sphere and set the scale
-                GameObject currentSoundInstance = Instantiate(SoundInstance, position, Quaternion.identity);
-                currentSoundInstance.transform.localScale = currentClip.size * Vector3.one;
+                GameObject currentSoundInstance = Instantiate(_soundInstance, position, Quaternion.identity);
+                // Setup variables for volume calculation
+                float distToPlayer = Vector3.Distance(position, _gameManager.PlayerInstance.position);
+                float volumeFactor = 1.0f - (distToPlayer / _maxDistToPlayer);
+                Vector3 dirToPlayer = _gameManager.PlayerInstance.position - position;
+
+                // Get all obstacles between sound source and player
+                RaycastHit[] hits;
+                hits = Physics.RaycastAll(position, dirToPlayer, dirToPlayer.magnitude, _obstacleMask);
+                foreach(RaycastHit hit in hits)
+                {
+                    // For reach obstacle, reduce the volume by 0.5
+                    volumeFactor /= 2.0f;
+                }               
+                // Assign the volume 
+                currentSoundInstance.GetComponent<AudioSource>().volume = volumeFactor;
                 // Play the sound
                 currentSoundInstance.GetComponent<AudioSource>().PlayOneShot(clipToUse);
                 // Destroy the sphere when the clip ends
---
+++ Assets/_Scripts/Test/TestGenerateSound.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGenerateSound : MonoBehaviour
+{
+    [SerializeField] private GameSoundController _soundController;
+    [SerializeField] private GameObject _soundSphere;
+    [SerializeField] private float _interval;
+
+    private void Start()
+    {
+        InvokeRepeating("TriggerSound", 0.0f, _interval);
+    }
+
+    private void TriggerSound()
+    {
+        _soundController.PlaySound("Wood Footstep", transform.position);
+    }
+}
